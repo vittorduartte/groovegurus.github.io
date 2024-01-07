7efa4fe3e657f2f0f0bf6c9c42b8268a72ed2b43
Mateus Vitor <mateusriograndense@gmail.com>
2024-01-07 14:51:35 -0300

    Corrigindo alinhamento e proporção da sessão de AboutUs

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,20 +1,20 @@
 <template>
   <section id="about-us">
-    <div class="flex flex-col justify-center py-24 lg:flex-row lg:items-center w-full">
+    <div class="flex flex-col justify-center py-20 lg:flex-row lg:items-center w-full">
         <div class="lg:mr-12">
-            <h1 class="font-regular text-5xl">SOBRE <span class="font-black">NÓS</span></h1>
+            <h1 class="font-regular text-brand-main text-4xl text-center">SOBRE <span class="font-black">NÓS</span></h1>
         </div>
-        <div class="flex flex-col lg:max-w-2xl">
-            <p class="font-regular text-3xl mt-12 px-14 leading-10 lg:text-justify">
-                Aqui, no coração do <span class="font-bold">Maranhão</span>, 
+        <div class="flex flex-col px-14 lg:max-w-3xl">
+            <p class="font-regular text-brand-main text-2xl mt-12 leading-10 text-center lg:text-justify">
+                Aqui, no coração do <span class="font-black">Maranhão</span>, 
                 estamos moldando o futuro da  música. 
-                Nossa missão é <span class="font-bold">unificar a cadeia que mantém o cenário musical</span> 
+                Nossa missão é <span class="font-black">unificar a cadeia que mantém o cenário musical</span> 
                 e construir uma plataforma colaborativa e sustentável. 
-                <span class="font-bold">Seja você parte dessa transformação!</span>
+                <span class="font-black">Seja você parte dessa transformação!</span>
             </p>
-            <div class="flex flex-col justify-center lg:flex-row justify-center mt-10 font-regular text-2xl lg:text-xl text-brand-black tracking-widest">
+            <div class="flex flex-col justify-center lg:flex-row lg:justify-start mt-10 font-regular text-2xl lg:text-base text-brand-black tracking-widest">
                 <img class="mr-5 mb-5 lg:mb-0 widget-state" src="../assets/images/maranhao-icon.svg" alt="">  
-                <p>PRIMEIRA <span class="font-bold mx-2">MUSICTECH</span> DO MARANHÃO</p>
+                <p class="text-center">PRIMEIRA <span class="font-bold mx-2">MUSICTECH</span> DO MARANHÃO</p>
             </div>
         </div>
     </div>
