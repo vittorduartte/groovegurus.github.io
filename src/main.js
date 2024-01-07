5b657d61b55dbb7f72d1a10fe7e4a4bbac021799
Mateus Vitor <mateusriograndense@gmail.com>
2024-01-07 14:51:00 -0300

    Importando ícones para o projeto

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCompass } from '@fortawesome/free-regular-svg-icons'
+import { faHandHoldingHeart } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faHandHoldingHeart)
+library.add(faCompass)
+
 import 'animate.css';
 import '@/assets/css/tailwind.css'
 import '@/assets/css/fonts.css'
 
 const app = createApp(App)
 app.use(store)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
