903b00fddd4ed756ca87bd2bb797b45feb751289
Mateus Vitor <mateusriograndense@gmail.com>
2024-01-07 14:53:23 -0300

    Removendo estilo nativo do projeto e importando componente Hero

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <Hero />
   <div class="about-us-research-sections">
     <AboutUs />
     <Research />
@@ -7,30 +8,23 @@
 </template>
 
 <script>
+import Hero from './components/Hero.vue';
 import AboutUs from './components/AboutUs.vue';
-import Footer from './components/Footer.vue';
 import Research from './components/Research.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: "App",
   components: {
-    Footer,
+    Hero,
+    AboutUs,
     Research,
-    AboutUs
+    Footer,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
 .about-us-research-sections {
   background-image: url('./assets/images/aboutus-research-bg.svg');
   background-size: cover;
