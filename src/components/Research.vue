8e79b8a7d6903912260185ca48931808accaf9c4
Mateus Vitor <mateusriograndense@gmail.com>
2024-01-06 22:20:27 -0300

    Mudando componente de redirecionamento de pesquisa para um anchor

--- src/components/Research.vue
+++ src/components/Research.vue
@@ -6,10 +6,10 @@
                 Dê voz à sua experiência e <br/>
                 participe da nossa pesquisa<span class="writing-machine">_</span> 
             </h3>
-            <button class="button-anchor hover:shadow-xl hover:shadow-mediumslateblue-400 transition-shadow duration-500">
+            <a href="#" class="button-anchor hover:shadow-xl hover:shadow-mediumslateblue-400 transition-shadow duration-500">
                 <img class="mr-3" src="../assets/icons/bx_user-voice.svg">
                 RESPONDER PESQUISA
-            </button>
+            </a>
         </div>
         <div class="hidden lg:inline-block">
             <img src="../assets/images/research-iillustration.svg" alt="">
@@ -28,6 +28,7 @@ export default {
 .button-anchor {
  @apply flex justify-center items-center text-white font-bold bg-brand-main p-4 mt-10 rounded-lg;
  letter-spacing: 0.2em;
+ cursor: pointer;
 }
 
 @keyframes blinking {
