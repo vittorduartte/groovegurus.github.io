fd8c412270a27bf1798b6671a3a59b8be96b543e
Mateus Vitor <mateusriograndense@gmail.com>
2024-01-13 16:56:13 -0300

    Corrigindo bug no componente marquee do gurupass

--- src/components/GuruPassInfinityScrool.vue
+++ src/components/GuruPassInfinityScrool.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)] mb-3">
-        <ul :class="`flex items-center justify-center md:justify-start [&_li]:mx-3 [&_img]:max-w-none animate-infinite-scroll${orientation === 'right'? 'inverse' : ''}`">
+        <ul :class="`flex items-center justify-center md:justify-start [&_li]:mx-3 [&_img]:max-w-none animate-infinite-scroll${orientation === 'right' ? 'inverse' : ''}`">
             <li>
                 <img src="../assets/images/guru-pass.svg" alt="GuruPass" />
             </li>
@@ -26,7 +26,7 @@
                 <img src="../assets/images/guru-pass.svg" alt="GuruPass" />
             </li>
         </ul>
-        <ul :class="`flex items-center justify-center md:justify-start [&_li]:mx-3 [&_img]:max-w-none animate-infinite-scroll${orientation === 'right'? 'inverse' : ''}`" aria-hidden="true">
+        <ul :class="`flex items-center justify-center md:justify-start [&_li]:mx-3 [&_img]:max-w-none animate-infinite-scroll${orientation === 'right' ? 'inverse' : ''}`" aria-hidden="true">
             <li>
                 <img src="../assets/images/guru-pass.svg" alt="GuruPass" />
             </li>
