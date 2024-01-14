fe9cbcae73ae3ca574387f68b0c2ff151ae89aa0
Mateus Vitor <mateusriograndense@gmail.com>
2024-01-14 10:56:42 -0300

    Ajustando espaçamentos e tamanhos em design responsivo

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="about-us">
-    <div class="flex flex-col justify-center py-20 lg:flex-row lg:items-center w-full">
-        <div class="lg:mr-12">
+    <div class="flex flex-col justify-center py-10 lg:py-20 lg:flex-row lg:items-center w-full">
+        <div class="my-10 lg:mr-12 lg:my-0">
             <h1 class="font-regular text-brand-main text-4xl text-center">SOBRE <span class="font-black">NÓS</span></h1>
         </div>
         <div class="flex flex-col px-14 lg:max-w-3xl">
@@ -13,7 +13,7 @@
                 <span class="font-black">Seja você parte dessa transformação!</span>
             </p>
             <div class="flex flex-col justify-center lg:flex-row lg:justify-start mt-10 font-regular text-2xl lg:text-base text-brand-black tracking-widest">
-                <img class="mr-5 mb-5 lg:mb-0 widget-state" src="../assets/images/maranhao-icon.svg" alt="">  
+                <img class="lg:mr-5 mb-5 lg:mb-0 widget-state" src="../assets/images/maranhao-icon.svg" alt="">  
                 <p class="text-center">PRIMEIRA <span class="font-bold mx-2">MUSICTECH</span> DO MARANHÃO</p>
             </div>
         </div>
