12cd6b46d55533fe8841d31d69c505de03b603ea
Mateus Vitor <mateusriograndense@gmail.com>
2024-02-08 17:49:11 -0300

    Configurando router, store e importando ícones para o projeto

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import router from './router'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faCompass } from '@fortawesome/free-regular-svg-icons'
-import { faHandHoldingHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHandHoldingHeart, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faHandHoldingHeart)
 library.add(faCompass)
+library.add(faXmark)
 
 import 'animate.css';
 import '@/assets/css/tailwind.css'
@@ -16,5 +18,6 @@ import '@/assets/css/fonts.css'
 
 const app = createApp(App)
 app.use(store)
+app.use(router)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
